704448aa541b6e9de7367870332a4e3f689462c1
Koki Ohira <koukng527@icloud.com>
2020-12-07 08:06:24 +0000

    Add some patials and views of footer
    Arrngement layouts and refactoring

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -9,12 +9,19 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get root" do
     get root_url
     assert_response :success
-  end
-  
-  test "should get home" do
-    get static_pages_home_url
-    assert_response :success
     assert_select "title", "#{@base_title}"
   end
-
+  
+  test "should get about" do
+    get about_path
+    assert_response :success
+    assert_select "title", "About | #{@base_title}"
+  end
+  
+  test "should get contact" do
+    get contact_path
+    assert_response :success
+    assert_select "title", "Contact | #{@base_title}"
+  end
+  
 end
