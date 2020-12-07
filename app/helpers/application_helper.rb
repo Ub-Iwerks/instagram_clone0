ccf5dceca00c7135e25dd4a1f1e30add1cc25497
Koki Ohira <koukng527@icloud.com>
2020-12-07 04:09:53 +0000

    Add full_title helper method
    Refactering display method of page title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  
+  #ページ毎に完全なタイトルを返す。
+  def full_title(page_title = "")
+    base_title = "Instagram_clone"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
+  
 end
