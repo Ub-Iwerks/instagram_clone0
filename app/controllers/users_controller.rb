12c187bee9919027e45356814935dcea9811d863
Koki Ohira <koukng527@icloud.com>
2020-12-11 23:21:53 +0000

    Implement login function through sessions controller
    sessions method inable to remember user's imfomation temporarily

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "Welcom to Instagram_clone App!!"
       redirect_to @user
     else
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -15,4 +15,10 @@ class ActiveSupport::TestCase
   include ApplicationHelper
 
   # Add more helper methods to be used by all tests here...
+  
+  #テストユーザーがログインしていたら、trueを返す。
+  def is_logged_in?
+    !session[:user_id].nil?
+  end
+  
 end
